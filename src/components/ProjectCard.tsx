a21476a5c7aa6bce3c8eeb479bafc12b47e34f72
Bruno Andrade <brubisandrade@hotmail.com>
2022-10-12 13:54:29 -0300

    fix-link

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image"
-import Link from "next/link"
 
 
 export const ProjectCard = ({title, description, imgUrl, url}) => {
     return (
         <>
             <div className="col-sm-6 col-md-4">
-                <Link href={url}>
+                <a href={url} rel="noopener noreferrer">
                     <div className="proj-imgbx">
                         <Image src={imgUrl} width={1500} height={1200} alt="" />
 
@@ -15,7 +14,7 @@ export const ProjectCard = ({title, description, imgUrl, url}) => {
                             <span> {description} </span>
                         </div>
                     </div>
-                </Link>
+                </a>
             </div>
         </>
     )
