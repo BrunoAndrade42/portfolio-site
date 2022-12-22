019d526367cbef39af9f58b1c0bed099aeaa54bf
Bruno Andrade <brubisandrade@hotmail.com>
2022-12-22 14:07:34 -0300

    att-cv

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -11,12 +11,12 @@ export const AboutMe = () => {
                             <Image src="/images/eu.jpg" className="eu-img" layout="intrinsic" width={400} height={400} alt="About Me" />
                         </div>
                         <div className="col-md-6">
-                            <h2> Sobre mim</h2>
+                            <h2> Sobre mim </h2>
                             <div className="row">
                                 <p>
                                     Olá, meu nome é Bruno e acredito que os dados podem ser utilizados para
                                     tomar melhores decisões. Possuo Tecnólogo em Ánalise e Desenvolvimento de Sistemas 
-                                    (Universidade Presbiteriana Mackenzie) e estou cursando Bacharelado em Ciências Econômicas
+                                    (Universidade Presbiteriana Mackenzie) e Graduação em Ciências Econômicas
                                     (Universidade Cruzeiro do Sul). Amo tecnologia e estou sempre me desenvolvendo e 
                                     aprendendo coisas novas! 
                                 </p>
